29b409f21a859743075a7bd6c0059fb0b9f3c4ac
HugoReyIn <hugo.rey@opendeusto.es>
2025-05-10 22:08:52 +0200

    Correction of more UnitTest

--- src/main/java/com/carrental/repository/BookingRepository.java
+++ src/main/java/com/carrental/repository/BookingRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.carrental.models.Booking;
@@ -23,6 +24,9 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     @Query("SELECT SUM(b.dailyPrice) FROM Booking b WHERE b.bookingStatus = :status")
     Double sumDailyPriceByBookingStatus(String status);
+
+    @Query("SELECT b FROM Booking b JOIN b.user u WHERE u.name = :name AND b.bookingStatus IN :statuses")
+    List<Booking> findByUser_NameAndBookingStatusIn(@Param("name") String name, @Param("statuses") List<String> statuses);
     
     List<Booking> findByUser(User user);
 }
