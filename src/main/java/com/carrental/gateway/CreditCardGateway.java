356b77dfa80d75a4f0214630b821124a552b1828
Unai Egusquiza <145966853+UnaiE@users.noreply.github.com>
2025-05-14 10:44:03 +0200

    Update CreditCardGateway.java NO system.out.println

--- src/main/java/com/carrental/gateway/CreditCardGateway.java
+++ src/main/java/com/carrental/gateway/CreditCardGateway.java
@@ -4,8 +4,8 @@ public class CreditCardGateway implements PaymentGateway {
 
     @Override
     public boolean processPayment(double amount, String customerDetails) {
-        System.out.println("Simulating credit card payment...");
-        System.out.println("Charged $" + amount + " to credit card of " + customerDetails);
+        //System.out.println("Simulating credit card payment...");  NO PRINT!
+        //System.out.println("Charged $" + amount + " to credit card of " + customerDetails);  NO PRINT!
         return true; // Simulación siempre exitosa
     }
 }
